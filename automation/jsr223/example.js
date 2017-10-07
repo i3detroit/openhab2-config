69694cb3517f7b14c02630c18fff001bf49f8ffb
root <root@mcclellan.i3detroit.local>
2017-10-07 01:38:08 -0400

    current config, somewhat functional

---
+++ automation/jsr223/example.js
@@ -0,0 +1,23 @@
+use strict';
+
+
+scriptExtension.importPreset("RuleSupport");
+scriptExtension.importPreset("RuleSimple");
+
+var sRule = new SimpleRule(){
+    execute: function( module, input){
+        print("Hello World from JavaScript");
+    }
+};
+
+sRule.setTriggers([
+    new Trigger(
+          "aTimerTrigger",
+          "timer.GenericCronTrigger",
+          new Configuration({
+                "cronExpression": "0/15 * * * * ?"
+																	            })
+    )
+]);
+
+automationManager.addRule(sRule);
